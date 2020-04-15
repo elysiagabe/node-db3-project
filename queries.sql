201180feab5c41b80c88b5b1b43bf0f540c5d284
Elysia Gabe <elysiagabe@gmail.com>
2020-04-15 15:01:36 -0700

    Implements helper methods in scheme-model.js

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ JOIN Category c ON p.CategoryId = c.id;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT o.Id
     , s.CompanyName
-FROM Order o
+FROM [Order] o
 JOIN Shipper s ON o.ShipVia = s.Id
 WHERE o.OrderDate < '2012-08-09';
 
@@ -26,6 +26,6 @@ ORDER BY p.ProductName;
 SELECT o.id as order_id
     , c.CompanyName as customer_company
     , e.LastName as employee_last_name
-FROM Order o
+FROM [Order] o
 JOIN Customer c ON o.CustomerId = c.id
-JOIN Employee e ON o.EmployeeId = e.id
+JOIN Employee e ON o.EmployeeId = e.id;
---
+++ schemes/scheme-model.js
@@ -0,0 +1,56 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    // SELECT * FROM schemes
+    return db('schemes');
+}
+
+function findById(id) {
+    // SELECT * FROM schemes WHERE schemes.id = id
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+    // in sql: 
+    // SELECT st.*, sch.scheme_name FROM schemes sch JOIN steps st ON sch.id = st.scheme_id WHERE schemes.id = id
+    return db.select('steps.id'
+        , 'schemes.scheme_name'
+        , 'steps.step_number'
+        , 'steps.instructions')
+        .from('schemes')
+        .join('steps', 'schemes.id', 'steps.scheme_id')
+        .where('schemes.id', id)
+        .orderBy('steps.step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme, 'id')
+        .then(([id]) => {
+            return findById(id)
+        })
+}
+
+function update(changes, id) {
+    return db('schemes').where({ id }).update(changes)
+    .then(() => {
+        return findById(id)
+    })
+}
+
+function remove(id) {
+    return findById(id)
+        .then(schemeToDelete => {
+            return db('schemes').where({ id }).del()
+                .then(() => schemeToDelete)
+        })
+}
